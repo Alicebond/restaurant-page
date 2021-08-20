3734d032837854d380932e6ee2ea74c28801c663
Zhibin <zzbsdh000@outlook.com>
2021-08-20 16:07:32 -0230

    Change margin of the description

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function component() {
   descrip.prepend(name);
   content.appendChild(name);
   descrip.classList.add("description");
-  p.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pulvinar quis turpis quis egestas. Aliquam bibendum dui porta porta auctor. Suspendisse a ipsum non sapien auctor ullamcorper. Vivamus eu quam non libero ultrices condimentum sed vel leo."
+  p.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pulvinar quis turpis quis egestas."
   descrip.appendChild(p);
   content.appendChild(descrip);
   // header.classList.add("header");
