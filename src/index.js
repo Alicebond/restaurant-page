18299e51c116e506cdaeb320d2f5d7e6ff24c6e2
Zhibin <zzbsdh000@outlook.com>
2021-08-30 17:09:30 -0230

    Add tab-switching logic

--- src/index.js
+++ src/index.js
@@ -14,25 +14,22 @@ import {
 } from "./footer.js"
 
 const content = document.querySelector("#content");
-content.prepend(createHeader());
+const body = document.querySelector("body");
+body.insertBefore(createHeader(), content)
 content.appendChild(hero());
-content.appendChild(createMenu());
-content.appendChild(createContact());
-content.appendChild(createFooter());
+content.after(createFooter());
 
-/*
 const nav = document.querySelector(".nav");
 nav.addEventListener("click", function (e) {
   e.preventDefault();
   if (e.target.textContent === "Home") {
-    content.childNodes[1].remove();
+    content.childNodes[0].remove();
     content.appendChild(hero());
   } else if (e.target.textContent === "Contact") {
-    content.childNodes[1].remove();
+    content.childNodes[0].remove();
     content.appendChild(createContact());
   } else if (e.target.textContent === "Menu") {
-    content.childNodes[1].remove();
+    content.childNodes[0].remove();
     content.appendChild(createMenu());
   }
 })
-*/
